623394918b86495f72a0fd249c43737a1abba702
Dell <Dell@sweety>
2025-11-20 21:24:44 +0530

    exception

---
+++ TNSIF/src/com/tnsif/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,11 @@
+//Program without exception handling
+package com.tnsif.exceptionhandling;
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
+
